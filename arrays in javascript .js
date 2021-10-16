0b1fa2739e82d3fbe3f3abb027374a010f93c84d
Dev prasanna <70941315+Devprasanna1905@users.noreply.github.com>
2021-10-16 10:30:17 +0530

    Create arrays in javascript .js

---
+++ arrays in javascript .js
@@ -0,0 +1,29 @@
+<html>
+	<head>
+	
+	
+	<title>Javascript</title>
+	<style>
+		
+	
+	
+	</style>
+	
+	</head>
+	<body>
+	
+
+			<script>
+				var arr= new Array();
+				arr[0]=1;
+				arr[1]=2;
+				arr[2]=3;
+				arr.push(12);
+				arr.splice(1,1,11)
+				console.log(arr);
+		
+		
+			</script>
+	
+	</body>
+</html>	
